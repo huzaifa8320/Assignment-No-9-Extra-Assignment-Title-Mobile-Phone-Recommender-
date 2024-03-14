cf43fa32aaa106da963ac4c653018694e376e69a
huzaifa8320 <huzaifa8320@gmail.com>
2024-03-14 12:42:19 +0500

    some changes

---
+++ Script/script.js
@@ -0,0 +1,13 @@
+var budget = prompt("Enter your budget" , "low/medium/high")
+var operator = prompt("Which operating system do you prefer?" , "Android/IOS")
+var camera = prompt("What camera quality do you need?" , "basic/moderate/high")
+var life = prompt("How important is battery life to you?" , "short/average/long")
+var screen = prompt("What screen size do you prefer?" , "small/medium/large")
+
+var androidbasic = "Samsung Galaxy A01"
+var androidmoderate = "Xiaomi Redmi 9A"
+var androidhigh = "Realme C11"
+
+var iosbasic = "iPhone SE (1st gen)"
+var iosmoderate= "iPhone XR"
+var ioshigh = "iPhone SE (1st gen)"
